e42469a7900fdebfec906b152448611c5f8ba5f4
Ellen <ellenmerchant@gmail.com>
2019-04-13 13:11:46 +0100

    Load SASS

---
+++ components/AppHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <header>
+    <h1 class="title">Ellen Merchant</h1>
+    <h4 class="subtitle">Qualified web developer with over {{ "TODO" }} industry experience</h4>
+  </header>
+</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,53 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'portfolio',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js project'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#3B8070'
+  },
+
+
+  css: [
+
+    '@/assets/css/app.scss'
+
+  ],
+
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
+     ** Run ESLint on save
+     */
+    extend(config, {
+      isDev,
+      isClient
+    }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
@@ -34,6 +57,9 @@ module.exports = {
         })
       }
     }
-  }
-}
+  },
 
+  plugins: [{
+    src: '~/plugins/vue-moment'
+  }],
+}
